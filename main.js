42bd2cd94b596c78341817db022c3f3c5001d7c3
YVIS TRINDADE DO NASCIMENTO <yvis.tnascimento@senacsp.edu.br>
2025-03-18 20:19:26 -0300

    cadastro

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ console.log("Electron - Processo principal")
 // importação dos recursos do framework
 // app (aplicação)
 // BrowserWindow (criação da janela)
-const { app, BrowserWindow } = require('electron/main')
+// nativeTheme está relacionado ao tema (claro ou escuro)
+// Menu (definir um menu personalizado)
+// shell (acessar links externos no navegador padrão)
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 
 // janela principal
 let win
 const createWindow = () => {
-   win = new BrowserWindow({
+  // definindo o tema da janela claro ou escuro
+  nativeTheme.themeSource = 'light'
+  win = new BrowserWindow({
     width: 1010,
     height: 720,
     //frame: false, // totem de pedido
@@ -18,8 +23,35 @@ const createWindow = () => {
     //autoHideMenuBar: true // esconder o menu
   })
 
+  // carregar o menu personalizado
+  // ATENÇÃO: Antes importar o recurso Menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
   // carregar o documento html na janela
   win.loadFile('./src/views/index.html')
+}
+
+// janela sobre
+let about
+function aboutWindow() {
+  nativeTheme.themeSource='light'
+  // obter a janela principal
+  const mainWindow = BrowserWindow.getFocusedWindow()
+  // validação (se existir a janela principal)
+  if (mainWindow) {
+    about = new BrowserWindow({
+      width: 320,
+      height: 280,
+      autoHideMenuBar: true,
+      resizable: false,
+      minimizable: false,
+      // estabelecer uma relação hierárquica entre janelas
+      parent: mainWindow,
+      // criar uma janela modal (só retorna a principal quando encerrada)
+      modal: true
+    })
+  }
+  about.loadFile('./src/views/sobre.html')
 }
 
 // inicialização da aplicação (assincronismo)
@@ -34,9 +66,74 @@ app.whenReady().then(() => {
   })
 })
 
+// Reduzir a verbozidade de logs não críticos (devtools)
+app.commandLine.appendSwitch('log-level', '3')
+
 // se o sistema não for MAC encerrar a aplicação quando a janela for fechada
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+// template do menu
+const template = [
+  {
+    label: 'Notas',
+    submenu: [
+      {
+        label: 'Criar nota',
+        accelerator: 'Ctrl+N',
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Sair',
+        accelerator: 'Alt+F4',
+        click: () => app.quit()
+      }
+    ]
+  },
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Aplicar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label: 'Reduzir zoom',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Restaurar zoom padrão',
+        role: 'resetZoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: "Recarregar",
+        role: "reload"
+      },
+      {
+        label: 'DevTools',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Repositório',
+        click: () => shell.openExternal('https://github.com/clxsilva/stickynotes')
+      },
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+    ]
+  }
+]
---
+++ src/views/renderer.js
@@ -0,0 +1,18 @@
+/**
+ * Processo de renderização do documento index.html
+ */
+
+console.log("Processo de renderização")
+// inserção da data no rodapé
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return data.toLocaleDateString('pt-BR', options)
+}
+
+document.getElementById('dataAtual').innerHTML = obterData()
